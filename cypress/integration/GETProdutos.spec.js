8381cdd29b5b40a52a5d0888bc13b684bdb31947
nawwarahprog <nawwarahyusof226@gmail.com>
2022-07-25 10:22:36 +0800

    First cypress project commit

---
+++ cypress/integration/GETProdutos.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('Products api', () => {
+    context('GET /products', () => {
+        it('should return a list with all products', () => {
+            cy.request({
+                method: 'GET',
+                url: 'https://serverest.dev/produtos'
+
+            })
+              
+               .should((response) => {
+                    expect(response.status).to.eq(200)
+                    expect(response.body.quantidade).to.eq(2)
+                    expect(response.body.produtos.length).to.be.eq(2);
+                    expect(response.body.produtos[0]).to.have.all.keys(
+                        'nome', 'preco', 'descricao', 'quantidade', '_id'
+                    )
+
+               });
+        });
+    });
+});
---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,8 @@
+
+describe("Mocha's interface", () => {
+    context('its provides a way to keep tests easier to read and organized.', () => {
+        it('This is your test case', () => {
+            expect(true).to.eq(true)
+        });
+    });
+});
